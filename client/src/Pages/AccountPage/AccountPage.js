35ad96d81d21aa5b213035888d46778817a42ba9
Neiz-Kap <ivan.mehedoff@yandex.ru>
2022-06-18 20:34:18 +0300

    Add logOut in AccountPage

--- client/src/Pages/AccountPage/AccountPage.js
+++ client/src/Pages/AccountPage/AccountPage.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { useCustomContext } from "./../../Hooks";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+import { COURSE_CATALOG_FULL_ROUTE } from "../../Utils/consts.js";
 
 const AccountPage = (props) => {
   const { user } = useCustomContext();
+  const navigate = useNavigate();
   const onLogout = (e) => {
     e.preventDefault();
+    user.setUser({});
+    user.setIsAuth(false);
+    navigate(COURSE_CATALOG_FULL_ROUTE);
   };
   return (
     <div className="account">
