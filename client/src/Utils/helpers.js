53ddfdbcdc2c486bab2a35ed8e511187fb4b707b
Neiz-Kap <ivan.mehedoff@yandex.ru>
2022-06-18 20:34:48 +0300

    Start CoursePage and start CourseContainer

--- client/src/Utils/helpers.js
+++ client/src/Utils/helpers.js
@@ -2,6 +2,6 @@ import { SERVER_LINK } from "./consts";
 export const getUrl = (links, isFullLink = false) => {
   return (
     (isFullLink ? SERVER_LINK : "") +
-    links.reduce((fullLink, link) => fullLink + link, 0)
+    links.reduce((fullLink, link) => fullLink + link)
   );
 };
